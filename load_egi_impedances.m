d619c2c7db57de8c4bcf74d8c25517e90918308b
Tommi Väyrynen <105491737+tommivayrynen1@users.noreply.github.com>
2022-12-12 09:20:54 +0200

    Update load_egi_impedances.m

--- load_egi_impedances.m
+++ load_egi_impedances.m
@@ -4,12 +4,12 @@ function impedances = load_egi_impedances(fullpath)
 % Impedances need to be separately saved in NetStation program during
 % measurements.
 
-insides = parseXML([fullpath '/info1.xml']);
-istruct = insides.Children(6).Children(4).Children(8).Children;
-ind2channel = find(strcmp({istruct.Name}','ch'));
-istruct = {istruct.Children}';
-istruct = istruct(ind2channel);
-icell = cellfun(@(x) x.Data, istruct, 'UniformOutput', false);
-impedances = cellfun(@(x) str2num(x), icell);
+  insides = parseXML([fullpath '/info1.xml']);
+  istruct = insides.Children(6).Children(4).Children(8).Children;
+  ind2channel = find(strcmp({istruct.Name}','ch'));
+  istruct = {istruct.Children}';
+  istruct = istruct(ind2channel);
+  icell = cellfun(@(x) x.Data, istruct, 'UniformOutput', false);
+  impedances = cellfun(@(x) str2num(x), icell);
 
 end
