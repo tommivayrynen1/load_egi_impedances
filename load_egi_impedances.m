253b1663182378d1c2b0893b91b0ee4c8255f037
Tommi Väyrynen <105491737+tommivayrynen1@users.noreply.github.com>
2022-12-12 09:20:29 +0200

    Update load_egi_impedances.m

--- load_egi_impedances.m
+++ load_egi_impedances.m
@@ -1,9 +1,8 @@
+function impedances = load_egi_impedances(fullpath)
+
 % Load impedances from egi raw file exports (.mff) folders.
 % Impedances need to be separately saved in NetStation program during
 % measurements.
-
-
-function impedances = load_egi_impedances(fullpath)
 
 insides = parseXML([fullpath '/info1.xml']);
 istruct = insides.Children(6).Children(4).Children(8).Children;
@@ -12,7 +11,5 @@ istruct = {istruct.Children}';
 istruct = istruct(ind2channel);
 icell = cellfun(@(x) x.Data, istruct, 'UniformOutput', false);
 impedances = cellfun(@(x) str2num(x), icell);
-
-
 
 end
